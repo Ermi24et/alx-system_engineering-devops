5810473a44bb698bbd37f07278aaf0afb8c8ae3f
Ermi24et <lilermi24@gmail.com>
2023-11-29 20:08:18 +0300

    install Nginx web server with puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,20 @@
 # configuring my server with puppet
 package { 'nginx':
   ensure => installed,
+  name   => 'nginx',
 }
 
 file_line { 'redirecting':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'listen 80 default_server;',
+  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  multiple => true
 }
 
 file { '/var/www/html/index.html':
   content => 'Hello World!',
+  path    => '/var/www/html/index.html'
 }
 
 service { 'nginx':
