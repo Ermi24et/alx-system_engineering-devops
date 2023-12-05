47174be15067b0065ace53025e8c5c5cfe341ea6
Ermi24et <lilermi24@gmail.com>
2023-12-06 00:52:10 +0300

    Add a custom HTTP header with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,34 @@
+# a manifest to install nginx
+
+class { 'nginx':
+  ensure => installed,
+}
+
+file { 'etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+    server {
+    	listen 80 default_server;
+    	listen [::]:80 default_server;
+    	add_header X-Served-By ${hostname};
+    	root /etc/nginx/html;
+    	index index.html index.htm;
+
+    	location /redirect_me {
+    	    return 301 https://www.youtube.com/watch?v=QH2-TGUwu4;
+    	}
+
+    	error_page 404 /404_error.html;
+    	location /404 {
+    	    root /etc/nginx/html;
+    	    internal;
+    	}
+    }
+  ",
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => File['/etc/nginx/sites-available/default'],
+}
