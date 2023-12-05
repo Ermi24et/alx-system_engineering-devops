ef3a788116233411f1b3cbdba2b8328c5ee77b1e
Ermi24et <lilermi24@gmail.com>
2023-12-06 01:11:52 +0300

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,34 +1,17 @@
-# a manifest to install nginx
-
-class { 'nginx':
-  ensure => 'installed',
+# manifest to install nginx
+exec { '/usr/bin/env apt-get -y update': }
+-> package { 'nginx':
+  ensure => installed,
 }
-
-file { 'etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "
-    server {
-    	listen 80 default_server;
-    	listen [::]:80 default_server;
-    	add_header X-Served-By ${hostname};
-    	root /etc/nginx/html;
-    	index index.html index.htm;
-
-    	location /redirect_me {
-    	    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    	}
-
-    	error_page 404 /404_error.html;
-    	location /404 {
-    	    root /etc/nginx/html;
-    	    internal;
-    	}
-    }
-  ",
+-> file { '/etc/nginx/html/index.html':
+  content => 'Hello World!',
 }
-
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => File['/etc/nginx/sites-available/default'],
+-> file_line { 'add header':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
+}
+-> service { 'nginx':
+  ensure => running,
 }
