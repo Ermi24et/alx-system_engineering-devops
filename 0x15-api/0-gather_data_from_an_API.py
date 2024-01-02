2dbbd2d259f7c2e9de8fc79a8d895171b7ca10f1
Ermi24et <lilermi24@gmail.com>
2024-01-02 21:39:54 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,8 +3,8 @@
 a script that using some REST API for a given employee ID returns information
 about his/her TODO list progress
 """
-import sys
 import requests
+import sys
 
 
 if __name__ == '__main__':
